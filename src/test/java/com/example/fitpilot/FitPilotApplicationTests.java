22000febebdfdaed22085386d4604da39e384dbd
Gulchin Taghizade <1pylr6h@code.edu.az>
2025-06-23 18:55:56 -0400

    Initial commit

---
+++ src/test/java/com/example/fitpilot/FitPilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitPilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
